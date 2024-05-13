c376e7b86d873df4b5c79f8f033f7912227225a8
Hossain Eshan <eshan.shahriar.77@gmail.com>
2024-05-14 07:58:35 +0800

    Integrated forms into login and signup pages, with a placeholder output

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app import app
+from app.forms import *
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('Welcome.html')
+    return render_template('welcomePage.html')
 
-@app.route('/pfp')
-def pfp():
-    return render_template('profilePage.html')
+@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('loginPage.html', form=form)
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash('Signup requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('signUpPage.html', form=form)
