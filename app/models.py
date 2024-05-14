955ab3b211196441a5246f1b243fb0b9c7685cba
Hossain Eshan <eshan.shahriar.77@gmail.com>
2024-05-14 21:05:19 +0800

    Added posts and comments to database

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
-
+    
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -11,5 +12,47 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
+    comments: so.WriteOnlyMapped['Comment'] = so.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    comments: so.WriteOnlyMapped['Comment'] = so.relationship(
+        back_populates='post')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+class Comment(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(280))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+    post_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Post.id),
+                                               index=True)
+    parent_comment_id: so.Mapped[Optional[int]] = so.mapped_column(
+        sa.ForeignKey('comment.id'))
+
+    author: so.Mapped[User] = so.relationship(back_populates='comments')
+    post: so.Mapped[Post] = so.relationship(back_populates='comments')
+    replies: so.WriteOnlyMapped['Comment'] = so.relationship(
+        backref=so.backref('parent', remote_side=[id])
+    )
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.body)
