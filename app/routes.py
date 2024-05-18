e310139e05036fbd1fe3de811fb76c32ca9af696
Hossain Eshan <eshan.shahriar.77@gmail.com>
2024-05-18 08:49:39 +0800

    Implemented user login, tested using manual db insertion

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, url_for, flash, redirect
 from app import app
 from app.forms import *
+from flask_login import current_user, login_user, logout_user
+import sqlalchemy as sa
+from app import db
+from app.models import User, Post, Comment
 
 @app.route('/')
 @app.route('/index')
@@ -9,12 +13,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
         return redirect(url_for('index'))
     return render_template('loginPage.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
