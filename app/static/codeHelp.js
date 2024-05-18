bd075848a93be87c604f0c314a11faf437f57dfa
Hossain Eshan <eshan.shahriar.77@gmail.com>
2024-05-18 11:29:40 +0800

    Added a simple dark theme toggle using css and jquery

---
+++ app/static/codeHelp.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    // Function to apply light theme
+    function enableLightTheme() {
+        $('body').removeClass('dark-theme');
+        $('.btn2').removeClass('btn-light').addClass('btn-dark');
+        $('.btn-home, .btn-ask, .btn-questions').removeClass('text-white');
+        localStorage.setItem('theme', 'light');
+    }
+
+    // Function to apply dark theme
+    function enableDarkTheme() {
+        $('body').addClass('dark-theme');
+        $('.btn2').removeClass('btn-dark').addClass('btn-light');
+        $('.btn-home, .btn-ask, .btn-questions').addClass('text-white');
+        localStorage.setItem('theme', 'dark');
+    }
+
+    // Handle checkbox change event
+    $('#themeToggle').change(function() {
+        if (this.checked) {
+            enableDarkTheme();
+        } else {
+            enableLightTheme();
+        }
+    });
+
+    // Check initial theme and apply it
+    if (localStorage.getItem('theme') === 'dark') {
+        $('#themeToggle').prop('checked', true);
+        enableDarkTheme();
+    } else {
+        enableLightTheme();
+    }
+});
